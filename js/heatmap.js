debea02d36649a0bfd3e60b7e63aa1826a066d9b
Mauro Del Rio <mauro@crs4.it>
2020-07-14 13:03:25 +0200

    first commit

---
+++ js/heatmap.js
@@ -0,0 +1,49 @@
+window.onload = function() {
+  this.viewer = OpenSeadragon({
+      id: "openseadragon1",
+      prefixUrl: "/openseadragon/images/",
+      tileSources: "http://mobydick.crs4.it/ome_seadragon/deepzoom/get/54.dzi"
+  });
+  var overlay = this.viewer.paperjsOverlay();
+
+  var defaultOpacity = 0.5;
+  opacityController = $('#opacity')[0];
+  opacityController.value = defaultOpacity*100;
+  
+
+  var layer = new paper.Layer();
+  layer.activate();
+  layer.opacity = defaultOpacity;
+
+
+
+  $('#opacity').on('input', function(){
+    layer.opacity = $(this).val()/100;
+     paper.project.view.update();
+  });
+
+  $.ajax({
+    'url': '/features/test.json',
+    datatype: 'json'
+  })
+    .done(function(patches){
+      console.log(patches[0]);
+      console.log(patches[0].size[0]);
+      patches.forEach(function(patch){
+        var cancer_percentage = patch.data.cancer_percentage;
+        if (cancer_percentage > 0) {
+          var heat = new paper.Rectangle(patch.x, patch.y, patch.size[0], patch.size[1]);
+          var path = new paper.Path.Rectangle(heat);
+          path.fillColor = new paper.Color(cancer_percentage*255, 0, 0);
+          // path.selected = true;
+        }
+
+      });
+
+
+
+    }
+
+    );
+};
+
